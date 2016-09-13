4df64b8ee4df5a4c61b7b1bbfe982d847592461d
Handsome Lin <b02705016@ntu.edu.tw>
2016-09-13 23:05:25 +0800

    Chatroom test (#3)
    * login and register and basic profile
    
    * complete register
    
    * finish chatroom and update register
    
    * add uploading files
    
    * finish team_list
    
    * use template
    
    * little syntax change of student_list.html
    
    * my stash
    
    * little fix
    
    * add team room
    
    * fix team room
    
    * add student's role and team's profile
    
    * add team applier but permission still need to solve
    
    * finish team apply and permission problem
    
    * little change
    
    * Master (#1)
    
    * add "install psycopg2"
    
    * Update README.md
    
    correct "in use"
    
    * Master (#2)
    
    * add "install psycopg2"
    
    * Update README.md
    
    correct "in use"

--- recruitsite/settings.py
+++ recruitsite/settings.py
@@ -16,7 +16,6 @@ from recruitsite.secret import DJANGO_SECRET_KEY
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
@@ -32,17 +31,20 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'profiles',
 ]
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -56,7 +58,9 @@ ROOT_URLCONF = 'recruitsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'templates/',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,5 +122,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_URL = '/static/img/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/img'),
+]
+#STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
-STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = "/index/"
+
+#one-to-one profile user
+#AUTH_PROFILE_MODULE = 'profiles.Student'
+
